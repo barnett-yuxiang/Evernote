73799967c07313af384fc132cce0c16fc299c787
yuxiang204 <yuxiang204@yeah.net>
2020-06-06 22:46:01 +0800

    export and module

--- Programmer/Platform_JavaScript/node/hello.js
+++ Programmer/Platform_JavaScript/node/hello.js
@@ -1,3 +1,14 @@
 exports.world = function () {
     console.log('Hello World\n');
 }
+
+function Hello() {
+    var name;
+    this.setName = function (thyName) {
+        name = thyName;
+    };
+    this.sayHello = function () {
+        console.log('Hello ' + name);
+    };
+};
+module.exports = Hello;
