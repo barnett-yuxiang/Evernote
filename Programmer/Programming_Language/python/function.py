55582dd788173f074521eb66104be7cae46e121d
yuli.kamakura <yuli.kamakura@bytedance.com>
2020-06-10 12:16:50 +0800

    PycharmProjects and function sample

---
+++ Programmer/Programming_Language/python/function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def hello():
+    print("Hello, Python!")
+
+hello()
+
+def area(width, height):
+    return width * height
+result = area(4, 6)
+print("result 4 * 6: ", result)
