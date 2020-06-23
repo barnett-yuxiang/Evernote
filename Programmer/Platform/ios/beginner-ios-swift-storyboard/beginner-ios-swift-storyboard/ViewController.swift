c5aa10651f6c1cce59f87b982395e3ad765646f5
Kamakura.Yx <yuxiang204@yeah.net>
2020-06-23 16:38:01 +0800

    iOS first

---
+++ Programmer/Platform/ios/beginner-ios-swift-storyboard/beginner-ios-swift-storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beginner-ios-swift-storyboard
+//
+//  Created by bytedance on 2020/6/22.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Programmer/Platform/ios/beginner-ios-swift-storyboard/beginner-ios-swift-storyboardTests/beginner_ios_swift_storyboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  beginner_ios_swift_storyboardTests.swift
+//  beginner-ios-swift-storyboardTests
+//
+//  Created by bytedance on 2020/6/22.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+import XCTest
+@testable import beginner_ios_swift_storyboard
+
+class beginner_ios_swift_storyboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
