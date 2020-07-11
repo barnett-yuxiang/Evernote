e606fffe8d54ec5bfee5b8e424fb12d6c46cd316
Kamakura.Yx <yuxiang204@yeah.net>
2020-07-11 20:02:31 +0800

    iOS start

---
+++ Programmer/Platform/iOS/EmptyWindow/EmptyWindow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmptyWindow
+//
+//  Created by bytedance on 2020/7/11.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Programmer/Platform/iOS/FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by bytedance on 2020/7/11.
+//  Copyright © 2020 bytedance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
