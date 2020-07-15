db6788b470c76b882a0a166c5cc5a8b6428de0f1
Kamakura.Yx <yuxiang204@yeah.net>
2020-07-15 16:47:05 +0800

    thread joint comment

--- Programmer/Programming_Language/cpp/thread_example/main.cpp
+++ Programmer/Programming_Language/cpp/thread_example/main.cpp
@@ -8,6 +8,8 @@ void task(std::string msg) {
 int main() {
 
   std::thread t1(task, "Hello");
+
+  // Makes the main thread wait for the new thread to finish execution, therefore blocks its own execution.
   t1.join();
 
   return 0;
