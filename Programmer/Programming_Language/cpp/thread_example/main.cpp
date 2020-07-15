12dccf6aa8424d7efc6a9ce8bdde48432f7eafdd
Kamakura.Yx <yuxiang204@yeah.net>
2020-07-15 17:04:04 +0800

    thread example 2

--- Programmer/Programming_Language/cpp/thread_example/main.cpp
+++ Programmer/Programming_Language/cpp/thread_example/main.cpp
@@ -2,15 +2,21 @@
 #include <thread>
 
 void task(std::string msg) {
-  std::cout << "task says: " << msg << std::endl;
+  std::cout << "task says: " << msg << " [" << std::this_thread::get_id() << "]" << std::endl;
 }
 
 int main() {
 
   std::thread t1(task, "Hello");
 
+  std::thread threadObj([] {
+    for (int i = 0; i < 5; i++)
+      std::cout << "Display Thread Executing [" << std::this_thread::get_id() << "]" << std::endl;
+  });
+
   // Makes the main thread wait for the new thread to finish execution, therefore blocks its own execution.
   t1.join();
+  threadObj.join();
 
   return 0;
 }
