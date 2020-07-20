c67e4123604c282261e6e199888d8d09a7cd427c
Kamakura.Yx <yuxiang204@yeah.net>
2020-07-20 18:59:07 +0800

    art ArenaObject

---
+++ Programmer/Aosp/art/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "libartbase/base/arena_object.h"
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+
+  art::ArenaObject arena_object = 101;
+
+  return 0;
+}
