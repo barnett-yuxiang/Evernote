73a081a7d51e0ea3695e6c43c98baaa75943120b
Kamakura.Yx <yuxiang204@yeah.net>
2020-08-09 10:28:07 +0800

    [Compiler] test cmp

---
+++ Programmer/Compiler/x86/test_cmp.c
@@ -0,0 +1,10 @@
+
+int main() {
+  int a = 10;
+  int b = 5;
+  if (a > 5) {
+    a = b;
+  }
+  int c = a + b;
+}
+
---
+++ Programmer/Compiler/x86/test_cmp.s
@@ -0,0 +1,32 @@
+	.file	"test_cmp.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$10, -12(%rbp)
+	movl	$5, -8(%rbp)
+	cmpl	$5, -12(%rbp)
+	jle	.L2
+	movl	-8(%rbp), %eax
+	movl	%eax, -12(%rbp)
+.L2:
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.12) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
