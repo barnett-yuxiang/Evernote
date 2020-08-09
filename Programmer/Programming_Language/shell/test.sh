34ca64ee6d1fe3695bc9821bbe021d6789510190
Kamakura.Yx <yuxiang204@yeah.net>
2020-08-09 17:08:08 +0800

    [shell] test

---
+++ Programmer/Programming_Language/shell/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#####################################################################
+# Print the message with the prefix INFO.
+#####################################################################
+function log_info() {
+  echo "INFO:" "$*"
+}
+
+log_info 'This is the main shell'
+
