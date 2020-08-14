c4c46080ce6847cd8d46046f55ca5ba709c33a83
Kamakura.Yx <yuxiang204@yeah.net>
2020-08-14 15:27:04 +0800

    [Python] effective python upload pdf

--- Programmer/Programming_Language/python/PycharmProjects/effective_python/ch01.py
+++ Programmer/Programming_Language/python/PycharmProjects/effective_python/ch01.py
@@ -12,3 +12,21 @@ if __name__ == '__main__':
 
     c = '于礼'
     print(c.encode('utf-8'))
+
+    key = 'my_var'
+    value = 1.234
+    formatted = '%-10s = %.2f' % (key, value)
+    print(formatted)
+
+    pantry = [
+        ('avocados', 1.25),
+        ('bananas', 2.5),
+        ('cherries', 15),
+    ]
+    for i, (item, count) in enumerate(pantry):
+        print('#%d: %-10s = %.2f' % (i, item, count))
+
+    old_way = '%-10s = %.2f' % (key, value)
+    print(old_way)
+    new_way = '%(key)-10s = %(value).2f' % {'key': key, 'value': value}
+    print(new_way)
