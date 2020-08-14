337be1a84eecdc1d5a8d58fc4848d8a0561cfc3d
Kamakura.Yx <yuxiang204@yeah.net>
2020-08-14 18:06:33 +0800

    [Python] effective python slicing

--- Programmer/Programming_Language/python/PycharmProjects/effective_python/ch01.py
+++ Programmer/Programming_Language/python/PycharmProjects/effective_python/ch01.py
@@ -30,3 +30,42 @@ if __name__ == '__main__':
     print(old_way)
     new_way = '%(key)-10s = %(value).2f' % {'key': key, 'value': value}
     print(new_way)
+
+    for i, (item, count) in enumerate(pantry):
+        before = '#%d: %-10s = %d' % (
+            i + 1,
+            item.title(),
+            round(count)
+        )
+
+        after = '#%(loop)d: %(item)-10s = %(count)d' % {
+            'loop': i + 1,
+            'item': item.title(),
+            'count': round(count)
+        }
+
+        assert before == after
+
+    formatted = f'{key!r:<10} = {value:.2f}'
+    print(formatted)
+
+    # item 6
+    favorite_snacks = {
+        'salty': ('pretzels', 100),
+        'sweet': ('cookies', 180),
+        'veggie': ('carrots', 20),
+    }
+
+    ((type1, (name1, cals1)),
+     (type2, (name2, cals2)),
+     (type3, (name3, cals3))) = favorite_snacks.items()
+
+    print(f'Favorite {type1:<10} is {name1:<10} with {cals1:<5} calories')
+    print(f'Favorite {type2:<10} is {name2:<10} with {cals2:<5} calories')
+    print(f'Favorite {type3:<10} is {name3:<10} with {cals3:<5} calories')
+
+    flavor_list = ['vanilla', 'chocolate', 'pecan', 'strawberry']
+    for i, flavor in enumerate(flavor_list, 10):
+        print(f'{i}: {flavor}')
+
+
---
+++ Programmer/Programming_Language/python/PycharmProjects/effective_python/ch02.py
@@ -0,0 +1,7 @@
+# Effective.Python.2nd.Edition.2019.11.
+# Ch02
+
+if __name__ == '__main__':
+    a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    print(a[-3:])
+    print(a[:-1])
