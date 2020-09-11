838129dab753e29364ae3071ff7f2d8f5c7e89f4
Kamakura.Yx <yuxiang204@yeah.net>
2020-09-11 22:14:43 +0800

    [Android] big commit
    Signed-off-by: Kamakura.Yx <yuxiang204@yeah.net>

---
+++ Programmer/Android/nanoscope-app/app/src/main/java/kamakura/yx/android/na/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package kamakura.yx.android.na.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kamakura.yx.android.na.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
