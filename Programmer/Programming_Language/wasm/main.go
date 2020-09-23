596e7a233020d6642ea0eabdbc6a30def9995291
Kamakura.Yx <yuxiang204@yeah.net>
2020-09-23 11:48:54 +0800

    [wasm] go with wasm
    Signed-off-by: Kamakura.Yx <yuxiang204@yeah.net>

--- Programmer/Programming_Language/wasm/main.go
+++ Programmer/Programming_Language/wasm/main.go
@@ -1,37 +1,7 @@
-// main.go
 package main
 
-import (
-	"syscall/js"
-	"fmt"
-	"time"
-)
-
-func bar(this js.Value, args []js.Value) interface{} {
-	t1 := time.Now()
-	fmt.Println("hellow wasm 1")
-	fmt.Println("hellow wasm 2")
-	fmt.Println("hellow wasm 3")
-	tc := time.Since(t1)	//计算耗时
-	fmt.Printf("time cost = %v\n", tc)
-	return -1
-}
-
-func registerCallbacks() {
-	js.Global().Set("bar", js.FuncOf(bar))
-}
+import "fmt"
 
 func main() {
-	c := make(chan struct{}, 0)
-	println("Hello, WebAssembly!")
-	registerCallbacks()
-
-	t1 := time.Now()
-	fmt.Println("hellow wasm 1")
-	fmt.Println("hellow wasm 2")
-	fmt.Println("hellow wasm 3")
-	tc := time.Since(t1)	//计算耗时
-	fmt.Printf("time cost = %v\n", tc)
-
-	<-c
+	fmt.Println("Hello, WebAssembly!")
 }
