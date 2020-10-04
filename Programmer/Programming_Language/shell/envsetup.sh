5d6067bda6ca5dcb99f225382d9fe3219001fbc1
Kamakura.Yx <yuxiang204@yeah.net>
2020-10-04 21:43:39 +0800

    [shell] update

--- Programmer/Programming_Language/shell/envsetup.sh
+++ Programmer/Programming_Language/shell/envsetup.sh
@@ -2,10 +2,30 @@
 
 
 
-function validate_current_shell() {
-    echo -e "validate current shell.\nUse of other shell would lead to erroneous results."
+function validate_current_shell()
+{
+    echo -e "validate current shell."
 
     type -t "$1"
 }
 
+function addcompletions()
+{
+    echo "addcompletions."
+    complete -F _lunch lunch
+}
+
+
+function _lunch()
+{
+    echo "_lunch."
+}
+
+function lunch()
+{
+    echo "lunch."
+}
+
+
 validate_current_shell
+addcompletions
