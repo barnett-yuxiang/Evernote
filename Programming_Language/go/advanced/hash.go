e8b9e6502c086f83d9bbc26ce69d358925e4e2d5
Kamakura.Yx <yuxiang204@yeah.net>
2020-12-17 14:44:50 +0800

    Get pointer value and to string
    Signed-off-by: Kamakura.Yx <yuxiang204@yeah.net>

--- Programming_Language/go/advanced/hash.go
+++ Programming_Language/go/advanced/hash.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	"hash/fnv"
+	"reflect"
 )
+
+type Stack struct {
+	Id int64
+}
 
 func Test03() {
 	fmt.Println(hash("HelloWorld"))
 	fmt.Println(hash("HelloWorld."))
+
+	stack := Stack{Id: 1024}
+	pointer := &stack
+	sp := fmt.Sprintf("%p", pointer)
+	fmt.Println(reflect.TypeOf(sp))
 }
 
 func hash(s string) uint32 {
