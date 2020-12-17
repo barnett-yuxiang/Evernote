a0d53ab6c293de058fd875a4ae042ffd5070a7b1
Kamakura.Yx <yuxiang204@yeah.net>
2020-12-17 14:23:48 +0800

    change dir
    Signed-off-by: Kamakura.Yx <yuxiang204@yeah.net>

---
+++ iOS/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by yuli on 2020/8/28.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+}
+
