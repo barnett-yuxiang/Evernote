ffdfbf27952d449fe3b8d6d6dedf36fcb92d4bf8
Kamakura <barnett.yuxiang@gmail.com>
2023-11-06 20:51:24 +0800

    101 monorepo with go and js projects

---
+++ Kamakura/workspace/project_build/example_bazel_01/main.go
@@ -0,0 +1,12 @@
+// main.go
+package main
+
+import "fmt"
+
+// main is the entry point of the Go program.
+//
+// It does not take any parameters.
+// It does not return any values.
+func main() {
+	fmt.Println("Hello, Bazel with Go!")
+}
